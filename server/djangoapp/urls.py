8826f5afec7df81233f2d6d79ab999aaa561099f
TimWongHP <timwonghp@gmail.com>
2022-07-22 18:31:53 +0000

    1st day

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,22 +7,30 @@ app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
+    path(route='index.html', view=views.home, name='home'),
+
     # name the URL
 
     # path for about view
+    path(route='about.html', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact.html', view=views.contact, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path(route='dealer/<int:id>/', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path(route='dealer/<int:id>/review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

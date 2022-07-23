833566b570604d78dcf93194c14b065c23bd6cd0
TimWongHP <66864819+TimWongHP@users.noreply.github.com>
2022-07-24 02:07:16 +0800

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", 'timwonghp.au-syd.mybluemix.net']
+ALLOWED_HOSTS = ["localhost", 'timwonghp.apic.au-syd.mybluemix.net']
 
 
 # Application definition

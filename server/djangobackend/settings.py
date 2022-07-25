ff2b8dbcde4a743b5eeae9394880e76a4d446ce7
TimWongHP <66864819+TimWongHP@users.noreply.github.com>
2022-07-25 20:23:40 +0800

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", 'https://timwonghp.au-syd.mybluemix.net', "https://timwonghp-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+ALLOWED_HOSTS = ["localhost", 'localhost:8000', 'https://timwonghp.au-syd.mybluemix.net', "https://timwonghp-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 CSRF_TRUSTED_ORIGINS = ['https://timwonghp-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 # Application definition
